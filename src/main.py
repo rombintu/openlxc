42798406a849f9b71f0b5a2b25f500d496c892da
Ваше Имя <you@example.com>
2021-08-26 13:30:48 +0300

    0.1.25

--- src/main.py
+++ src/main.py
@@ -11,12 +11,20 @@ from pylxd import Client
 import json
 import os
 
+
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret123'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
 
 # main = Blueprint('main', __name__)
+
+
+client = Client(endpoint=os.getenv("SERVERLXD"),
+                cert=(os.getenv("CERTPATH"), os.getenv("KEYPATH")))
+client.authenticate(os.getenv("SERVERSEC"))
+# client.trusted
 
 # @login_required
 @app.route('/')
@@ -41,35 +49,43 @@ def index():
         uname['machine'],
         uname['processor']
         )
-    client = Client()
-    containers = client.containers.all()
-    return render_template('index.html', containers=containers, hypervisor=hypervisor)
+
+    containers = []
+
+    try:
+        # client = Client(endpoint=SERVERLXD)
+        # client.authenticate(SERVERSEC)
+        # client.trusted
+        containers = client.instances.all()
+    except Exception as e:
+        print(e)
+    return render_template('index.html', containers=containers, hypervisor=hypervisor) 
 
 @app.route('/container/<name>', methods=['POST', 'GET'])
 def container(name):
-    client = Client()
+    # client = Client()
     container = client.containers.get(name)
     if request.method == 'POST':
         container.start()
         return redirect(url_for('index'))
     return render_template('container.html', container=container)
 
-@app.route('/add_iso')
+@app.route('/upload/iso')
 def add_iso():
     if request.method == 'POST':
         name = request.form['name']
         desc = request.form['desc']
-        distr = request.form['distr']
-        arch = request.form['arch']
-        version = request.form['version']
-        public = True if request.form.get('public') else False
+        # distr = request.form['distr']
+        # arch = request.form['arch']
+        # version = request.form['version']
+        # public = True if request.form.get('public') else False
 
-        if name == '' or distr == '' or arch == '' or version == '':
+        if name == '':
             flash('Поля должны быть заполнены')
-            return redirect(url_for('add_iso'))
-    return render_template('add_iso.html')
+            return redirect(url_for('upload_iso'))
+    return render_template('create_vm.html')
 
-@app.route('/create_vm_local', methods=['POST', 'GET'])
+@app.route('/create/vm', methods=['POST', 'GET'])
 def create_vm_local():
     if request.method == 'POST':
         name = request.form['name']
@@ -83,22 +99,22 @@ def create_vm_local():
             config = {
                 'name': name, 
                 'source': {'type': 'image', 'alias': iso}}
-            client = Client()
+            # client = Client()
             instance = client.instances.create(config, wait=True)
             instance.start()
         except Exception as e:
             flash(str(e))
-            return redirect(url_for('create_vm_local'))
+            return redirect(url_for('create/vm'))
 
         return redirect(url_for('index'))
-    client = Client()
+    # client = Client()
     images = client.images.all()
-    return render_template('create_vm_local.html', images=images)
+    return render_template('create_vm.html', images=images)
 
-@app.route('/create_vm_url', methods=['POST', 'GET'])
+@app.route('/download/iso', methods=['POST', 'GET'])
 def create_vm_url():
     if request.method == 'POST':
         pass
     
-    return render_template('create_vm_url.html')
+    return render_template('download_iso.html')
 
